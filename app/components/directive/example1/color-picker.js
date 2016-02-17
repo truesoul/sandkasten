956d01a07b2606245080fbbff626b2ea8b639a2c
truesoul <mr.summersky@gmx.de>
2016-02-17 10:00:40 +0100

    Color Picker fertig implementiert.

--- app/components/directive/example1/color-picker.js
+++ app/components/directive/example1/color-picker.js
@@ -6,9 +6,16 @@
   function colorPickerDirective() {
     return {
       restrict: 'E',
+      /*scope: {
+          r: '@',
+          g: '@',
+          b: '@'
+      },*/
       templateUrl: './components/directive/example1/color-picker.html',
       controller: function ($scope) {
-
+        $scope.r = $scope.r || Math.random() * 255;
+        $scope.g = $scope.g || Math.random() * 255;
+        $scope.b = $scope.b || Math.random() * 255;
       }
     }
   }
